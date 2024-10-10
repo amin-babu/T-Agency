be08109cc2a8843f371fe717344169ea616cf4f3
aminbabu12 <badrulaminbabu@gmail.com>
2024-10-10 15:55:19 -0400

    responsive navber finished

--- main.js
+++ main.js
@@ -6,5 +6,10 @@ menuBtn.addEventListener('click', (e) => {
   navLinks.classList.toggle('open');
 
   const isOpen = navLinks.classList.contains('open');
-  menuBtnIcon.setAttribute('class', isOpen ? "ri-close-line" : "ri-menu-line")
+  menuBtnIcon.setAttribute('class', isOpen ? "ri-close-line" : "ri-menu-line");
 });
+
+navLinks.addEventListener('click', (e) => {
+  navLinks.classList.remove("open")
+  menuBtnIcon.setAttribute('class', 'ri-menu-line')
+})
